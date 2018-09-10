3a9bfc486b697c09d2bbf6e03ea486059bb3efb7
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2018-09-10 17:21:23 +0200

    hovering plot 1

--- hough_new.js
+++ hough_new.js
@@ -8,7 +8,8 @@ function create_svg(id, x_range, y_range, x_label, y_label){
 
 	svg = d3.select(id)
 					.append("svg")
-						.attr("viewBox", "0 0 "+ w + " " + h);
+						.attr("viewBox", "0 0 "+ w + " " + h)
+						.attr("cursor", "crosshair")
 
   // scales
   let xScale = d3.scaleLinear()
@@ -56,16 +57,104 @@ function create_svg(id, x_range, y_range, x_label, y_label){
 	    .attr("font-size", "10pt")
 	    .text(y_label);
 
-  return svg.node();
+  return [svg, xScale, yScale];
+}
+
+// function performing the actual hough transform
+function r(theta, x, y) {
+	return x * Math.cos(theta*2*Math.PI/360) + y * Math.sin(theta*2*Math.PI/360);
 }
 
 function insert_hough_plots(svg1_id, svg2_id){
 	let x_range = [-10, 10]
 	let x_range2 = [0, 360]
 	let y_range = [10, -10]
-	let svg1 = create_svg(svg1_id, x_range, y_range, "x", "y")
-	let svg2 = create_svg(svg2_id, x_range2, y_range, "Θ", "d")
-  //let svg2 = create_svg(svg2_id, color2, 400)
+	let [svg1, xScale1, yScale1] = create_svg("#"+svg1_id, x_range, y_range, "x", "y")
+	let [svg2, xScale2, yScale2] = create_svg("#"+svg2_id, x_range2, y_range, "Θ", "d")
+
+	let data = [[5, 5, "black"]];
+	let next_idx = 0
+
+	data.forEach(p => insert_point(xScale1(p[0]), yScale1(p[1])))
+
+	svg1.on("click", function(){
+	  insert_point(d3.mouse(this)[0], d3.mouse(this)[1])
+  });
+
+	function insert_point(x, y) {
+		idx = next_idx++
+		color = d3.schemeCategory10[idx%10]
+		// point in svg1
+		svg1.append("circle")
+		    .attr("id", svg1_id + "-" + idx)
+				.attr("cx", x)
+        .attr("cy", y)
+        .attr("r", 10)
+        .attr("cursor", "move")
+        .attr("fill", color)
+        .on("click", delete_point)
+        .call(d3.drag()
+                .on("drag", dragged))
+		// line in svg2
+		svg2.append("path")
+        .attr("id", svg1_id + "-" + idx + "-line")
+        .datum(d3.range(x_range2[0], x_range2[1], 1))
+        .attr("fill", "none")
+        .attr("stroke", color)
+        .attr("stroke-width", 1.5)
+        .attr("d", gen_hough_line(x, y))
+	}
+
+	function gen_hough_line(x, y){
+		return d3.line()
+			.x(theta => xScale2(theta))
+			.y(theta => yScale2(
+					r(theta, xScale1.invert(x), yScale1.invert(y))
+				)
+			);
+	}
+
+	function delete_point(){
+    d3.event.stopPropagation();
+    d3.select(this).remove();
+		d3.select("#" + this.id + "-line").remove();
+  }
+
+	function dragged(d) {
+    let x = Math.max(xScale1(-10), Math.min(xScale1(10), d3.event.x));
+    let y = Math.max(yScale1(10), Math.min(yScale1(-10), d3.event.y));
+		// update point
+		d3.select(this).attr("cx", x).attr("cy", y);
+		// update line
+		d3.select("#" + this.id + "-line").attr("d", gen_hough_line(x, y))
+  }
+
+
+  svg1.on("mouseenter", svg1MouseEnter)
+	    .on("mousemove", svg1MouseMove)
+			.on("mouseleave", svg1MouseLeave)
+
+	function svg1MouseEnter(){
+    svg2.append("path")
+          .attr("id", svg1_id + "-hover-line")
+          .datum(d3.range(x_range2[0], x_range2[1], 1))
+          .attr("fill", "none")
+          .attr("stroke", "grey")
+					.style("stroke-dasharray", "3, 3")
+          .attr("stroke-width", 1.5)
+          .attr("d", gen_hough_line(d3.mouse(this)[0], d3.mouse(this)[1]))
+  }
+
+	function svg1MouseMove(){
+		d3.select("#" +  svg1_id + "-hover-line")
+		  .attr("d", gen_hough_line(d3.mouse(this)[0], d3.mouse(this)[1]))
+	}
+
+	function svg1MouseLeave(){
+		d3.select("#" +  svg1_id + "-hover-line").remove();
+	}
+
+	//let svg2 = create_svg(svg2_id, color2, 400)
  	/*let svg2_div = document.createElement("div")
   svg2_div.classList.add("hough-wrapper")
   let canvas = document.createElement("canvas")
